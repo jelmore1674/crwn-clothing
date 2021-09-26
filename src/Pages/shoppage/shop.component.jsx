7049ab126772ff2c1c67aa8231ef00eb553f4e5f
Justin Elmore <jelmore1674@pm.me>
2021-09-26 15:14:23 -0500

    added shop, and directory to redux, added collectionsOverview component

--- src/Pages/shoppage/shop.component.jsx
+++ src/Pages/shoppage/shop.component.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../Components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../Components/collections-overview/components-overview.component';
 
-class ShopPage extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			collections: SHOP_DATA,
-		};
-	}
-
-	render() {
-		const { collections } = this.state;
-		return (
-			<div className='shop-page'>
-				{collections.map(({ id, ...otherCollectionProps }) => (
-					<CollectionPreview key={id} {...otherCollectionProps} />
-				))}
-			</div>
-		);
-	}
-}
+const ShopPage = ({ collections }) => {
+	return (
+		<div className='shop-page'>
+			<CollectionsOverview />
+		</div>
+	);
+};
 
 export default ShopPage;
